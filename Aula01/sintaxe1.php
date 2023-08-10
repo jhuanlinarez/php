95d0bfaf69767dcf1a7a864c831e99fb9da40617
jhuanlinarez <linarezjhuan721@gmail.com>
2023-08-10 14:31:53 -0300

    php

---
+++ Aula01/sintaxe1.php
@@ -0,0 +1,10 @@
+<?php
+    
+    //geração de texto(string)
+    echo "Chama Sesi-Senai !";
+
+    // Geração de texto estruturado (com tags, atributos)
+    echo "<h2>Gerando <span class='destaque'>HTML</span> através do PHP. </h2>";
+    echo "<h2>Gerando <span class=\"destaque\">HTML</span> através do PHP. </h2>";
+
+?>
---
+++ Aula01/sintaxe3.php
@@ -0,0 +1,14 @@
+<?php
+     echo               "Testando espaço em branco <br>";
+     echo "testando espaço <br>"           ;
+
+     //O carater &nbsp; adiciona um espaço em branco dentro da string
+
+     echo "&#160;  &#160;                                testando        espaço    em    branco <br>";
+     echo
+     "quebra de linha <br>"
+     ;
+
+     echo "quebra de linha <br>"
+     ;
+?>
