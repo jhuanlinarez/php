45e57e65933f37727adca2006431f34ade21bf9c
jhuanlinarez <linarezjhuan721@gmail.com>
2023-10-05 11:41:55 -0300

    aa

--- Aula04_alunos/3_formulario/10-formulario-processamento.php
+++ Aula04_alunos/3_formulario/10-formulario-processamento.php
@@ -4,48 +4,72 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Link CSS -->
-    <link rel="stylesheet" href="../style.css">
-    <title>Formulario e processamento</title>
+    <title>Processamento POST</title>
 </head>
 <body>
-    <h1 class="destaqueP">Ex_10 - Formulario e processamento</h1>
+    <h1>Processamento de dados usando POST</h1>
     <hr>
+    <!-- Método mais seguro (Não mostra as informações na URL) -->
+    <?php
 
-    <!-- ________________________________________________________________________________ -->
- 
+    //SE + OU (||(Quando o usuário não preencher nome e email))
+    if(empty($_POST['nome']) || empty($_POST['email'])) {
+?>
+<p style="color:red">Você deve preencher nome e e-mail!</p>
 
-    <!-- 1ª Digitação (aqui) -->
+<?php
 
-    <section>
-        <h2>Dados</h2>
-        <p>Nome: <?= $nome?></p>
-        <p>E-mail: <?= $email?></p>
-    </section>
-    
-    <p><a href=""><link href="10-formulario-processamento.php">Voltar</a></p>
-    
+} else {
+    //Captura dos dados com filtro de segurança
+    $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    $idade = filter_input(INPUT_POST, 'idade', FILTER_SANITIZE_NUMBER_INT);
+    $mensagem = filter_input(INPUT_POST, 'mensagem', FILTER_SANITIZE_SPECIAL_CHARS);
+    $informativos = filter_input(INPUT_POST, 'informativos', FILTER_SANITIZE_SPECIAL_CHARS);
 
-    <!-- 2ª Digitação (aqui) -->
+    //Operador de coalescência nula (Verifica se o que está a esquerda existe (interesses), senão pega o da direita, neste caso vazio [])
+        $interesses = filter_input(INPUT_POST, 'escolha', FILTER_SANITIZE_SPECIAL_CHARS);
+   
+?>
+<h2>Dados</h2>
 
-    
-    <!-- Entra aqui porque ao entrar na página ele entra no falso e cai aqui (Mostra formulário) -->
+<ul>
+    <li>Nome: <?=$nome?></li>
+    <li>E-mail: <?=$email?></li>
+    <li>Nome: <?=$idade?></li>
+    <li>Nome: <?=$informativos?></li>
 
-    <!-- Deixe o action vazio! -->
-    <form action="" method="post">
-        <p>
-            <label for="nome">Nome:</label>
-            <input type="text" name="nome" id="nome">
-        </p>
-        <p>
-            <label for="email">E-mail:</label>
-            <input type="email" name="email" id="email">
-        </p>
-        <button type="submit" name="enviar">Enviar</button>
-        <button type="reset" name="limpar">Limpar</button>
-    </form>
+    <!--_____________________________________________________________________  -->
 
-    <!-- 3ª Digitação (aqui) -->
-    
+    <!-- Condicional SE (Não) estiver vazio executa. (Uso SE Simples) -->
+    <?php if(!empty($interesses)) { ?>
+        <!-- Transforma um array em string (abaixo) -->
+        <!-- Opção 1: Implodir o array, transformando em String -->
+        <li>Interesses: <?=implode(",",$interesses)?></li>
+
+        <!-- _______________________________________________________________________ -->
+
+        <li>Interesses:
+            <ul>
+                <!-- Opção 2: Acessar o array usando loop -->
+                <?php foreach($interesses as $interesse) { ?>
+                    <li><?=$interesse?></li>
+                <?php } ?>
+            </ul>
+        </li>
+    <?php } ?>
+
+                    <li>Escolha: <?=$escolha?></li>
+                    <li>Mensagem: <?=$mensagem?></li>
+   
+</ul>
+
+<?php
+} // fim if/else da validação de campos obrigatórios
+?>
+
+
+
+   
 </body>
 </html>
